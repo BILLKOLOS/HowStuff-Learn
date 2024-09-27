f022b742a37b1189d36dab12c32dbec2b23db83c
billkolos <billooko2@gmail.com>
2024-09-27 20:17:39 +0300

    dash

---
+++ HowStuff-Learn-App/HowStuff-Learn-App/backend/routes/dashboardRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const dashboardController = require('../controllers/dashboardController');
+const authMiddleware = require('../middleware/authMiddleware'); // To protect routes
+
+// Get dashboard data
+router.get('/dashboard', authMiddleware, dashboardController.getDashboard);
+
+module.exports = router;
