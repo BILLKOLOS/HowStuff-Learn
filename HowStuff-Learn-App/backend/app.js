40fc2166be851680f615f1f0cd5021164361b242
billkolos <billooko2@gmail.com>
2024-10-08 19:31:57 +0300

    string interpolation fixed

--- HowStuff-Learn-App/backend/app.js
+++ HowStuff-Learn-App/backend/app.js
@@ -73,5 +73,5 @@ app.use((req, res, next) => {
 // Server setup
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(Server running on port ${PORT});
+    console.log(`Server running on port ${PORT}`);
 });
